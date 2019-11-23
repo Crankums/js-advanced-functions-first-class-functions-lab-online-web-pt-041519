f99f75cf2ed518d29456c4fa213b2e6160553d5e
Craig Donavin <crdonavin@gmail.com>
2019-11-23 02:22:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const returnFirstTwoDrivers = function(arr) {
   let returnArr = arr.slice(0,2)
   return returnArr
 }
+
+const returnLastTwoDrivers = function(arr) {
+  let returnArr = arr.slice(-2,2)
+}
