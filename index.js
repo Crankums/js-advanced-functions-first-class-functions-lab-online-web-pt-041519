8bee2199d75b3377e8f97f81f079d5692180d47c
Craig Donavin <crdonavin@gmail.com>
2019-11-23 02:32:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const returnLastTwoDrivers = function(arr) {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
-createFareMultiplier(int) {
+function createFareMultiplier(int) {
   return function(fare) {
     return fare*int
   }
